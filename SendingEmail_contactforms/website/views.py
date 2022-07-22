31b176a1e99e2ccd6dce843aa78e48af1b2f5966
kavin <srijanrcis@gmail.com>
2022-07-22 12:42:52 +0530

    initial commit

---
+++ SendingEmail_contactforms/website/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# Create your views here.
+def contact(request):
+    if request.method=='POST':
+        print("helloo")
+        name =request.POST.get('FullName')
+        phonenumber =request.POST.get('PhoneNumber')
+        email =request.POST.get('Email')
+        message =request.POST.get('message')
+
+        data={
+            'name':name,
+            'phonenumber':phonenumber,
+            'email':email,
+            'message':message,
+
+        }
+        print(data)
+        message=("From {} /n".format('email')+data['message'])
+        send_mail(data['phonenumber'],message,data['email'],['kavinprojectsmpt@gmail.com'])
+        
+
+    return render(request, 'contact.html',{})
